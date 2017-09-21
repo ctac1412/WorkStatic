1ad561d50c4d3b1021789cb307042596bd09fb6a
ctac1412 <dom333122@yandex.ru>
2017-09-21 18:53:08 +0300

    Full work go

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,57 +1,116 @@
-browser.tabs.onActivated.addListener(updateContent);
-function updateContent() {
-  console.log("загрузились");
+(function(obj) {
 
-  // browser.tabs.query({windowId: ActiveTab(), active: true})
-  //    .then((tabs) => {
-  //      console.log(tabs[0].url);
-  //      return
-  // })
-}
+	var requestFileSystem = obj.webkitRequestFileSystem || obj.mozRequestFileSystem || obj.requestFileSystem;
 
-document.querySelector('#s_save').addEventListener('click', function(e) {
-  browser.tabs.query({windowId: ActiveTab(), active: true})
-     .then((tabs) => {
-      //  browser.tabs.reload();
+	function onerror(message) {
+    console.log(message);
+		alert(message);
+	}
 
-      //  d = new FormData();
-      //  d.append('Key',"Value");
-      //
-      //  fetch(tabs[0].url,{
-      //    method : 'GET',
-      //    credentials : 'include',
-      //    body : d
-      //  }).then((response)=>{
-      //    console.log("response.status", response.status);
-      //  });
-      //  console.log();
+	function createTempFile(callback) {
+		var tmpFilename = "tmp.dat";
+		requestFileSystem(TEMPORARY, 4 * 1024 * 1024 * 1024, function(filesystem) {
+			function create() {
+				filesystem.root.getFile(tmpFilename, {
+					create : true
+				}, function(zipFile) {
+					callback(zipFile);
+				});
+			}
 
-      browser.tabs.sendMessage(
-            tabs[0].id,{
-            message : "Hello"}
-          ).then((response)=>{
-            // response.querySelector('#ProductInfo').value;
-          // console.log(response);
-          });
+			filesystem.root.getFile(tmpFilename, null, function(entry) {
+				entry.remove(create, create);
+			}, create);
+		});
+	}
 
-  })
-});
+	var model = (function() {
+		var URL = obj.webkitURL || obj.mozURL || obj.URL;
+
+		return {
+			getEntries : function(file, onend) {
+        var tmp=new zip.BlobReader(file);
+				zip.createReader(tmp, function(zipReader) {
+					zipReader.getEntries(onend);
+				}, function (error) {
+         onerror(error)
+        });
+
+			},
+			getEntryFile : function(entry, creationMethod, onend, onprogress) {
+				var writer, zipFileEntry;
+
+				function getData() {
+					entry.getData(writer, function(blob) {
+						var blobURL = creationMethod == "Blob" ? URL.createObjectURL(blob) : zipFileEntry.toURL();
+
+						onend(blobURL);
+					}, onprogress);
+				}
+
+				if (creationMethod == "Blob") {
+					writer = new zip.BlobWriter();
+					getData();
+				} else {
+					createTempFile(function(fileEntry) {
+						zipFileEntry = fileEntry;
+						writer = new zip.FileWriter(zipFileEntry);
+						getData();
+					});
+				}
+			}
+		};
+	})();
+
+	(function() {
+		var fileInput = document.getElementById("file-input");
+    var btnStart = document.querySelector('#start_load');
+		// var unzipProgress = document.createElement("progress");
+		// var fileList = document.getElementById("file-list");
+		// var creationMethodInput = document.getElementById("creation-method-input");
 
 
-// btn2.onclick = function(event){
-//    if (document.querySelector("#btnSave") == null) {
-//      alert("Ни одна кнопка 'Сохранить' не найдена.")
-//    } else {
-//      document.querySelector("#btnSave").click()
-//      change_status = false
-//
-//    };
-//  };
+//NOTE: LISTINER CHANGE
 
+    btnStart.addEventListener('click', function() {
+      var arrpromise = []
+			model.getEntries(fileInput.files[0], function(entries) {
+				entries.forEach(function(entry) {
+          if (entry.filename == "xl/worksheets/sheet1.xml" || entry.filename == "xl/sharedStrings.xml"){
+            arrpromise.push(readfile(entry))
+          }
+				});
+        Promise.all(arrpromise).then(()=>{
+          starting()
+        });
+			});
 
+		}, false);
 
-function ActiveTab() {
-  browser.windows.getCurrent({populate: true}).then((windowInfo) => {
-    return  windowInfo.id;
+function readfile(entry) {
+  return  new Promise(function(resolve, reject) {
+    var writer = new zip.BlobWriter();
+    entry.getData(writer, function(blob) {
+
+        var reader = new FileReader();
+        reader.onload = function(event) {
+          switch (entry.filename) {
+            case "xl/worksheets/sheet1.xml":
+              Sheet1.text =  event.target.result
+            break;
+            case "xl/sharedStrings.xml":
+              sharestring.text =  event.target.result
+            break;
+          }
+          resolve()
+        }
+        reader.readAsText(blob)
+    })
   });
+
+
 }
+
+	})();
+
+})(this);
